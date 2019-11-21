b382c7e92d1934419446b536c5ae73e668bac8fa
dima <dima.sapsay@gmail.com>
2019-11-21 22:48:54 +0200

    second commit

--- scalar.py
+++ scalar.py
@@ -4,13 +4,13 @@ Find scalar product of two vectors
 
 from typing import List
 
-
-def scalar_product(list_a, list_b):
+def scalar_product(list_a: List[float], list_b: List[float]) -> int:
     """ Scalar product """
     scal_sum = 0
 
     if list_a:
-        for i in range(len(list_a)):
+        len_list = len(list_a)
+        for i in range(len_list):
             scal_sum += int(list_a[i] * list_b[i])
 
     return scal_sum
