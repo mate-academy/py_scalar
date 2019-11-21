31f167511266ea067f01bda98599c22f48a2e502
logotip <logotip123@yahoo.com>
2019-11-21 11:01:04 +0200

    v 1.0

--- scalar.py
+++ scalar.py
@@ -1,6 +1,17 @@
+"""scalar vector product module"""
 from typing import List
 
 
-def scalar_product(a: List[float], b: List[float]) -> List[float]:
-    return 0
-
+def scalar_product(first_vector: List[int], second_vector: List[int]) -> int:
+    """scalar vector product function"""
+    lenght_first, lenght_seond = len(first_vector), len(second_vector)
+    if lenght_first == 0 or lenght_seond == 0:
+        return 0
+    if lenght_first < lenght_seond:
+        count = lenght_first
+    else:
+        count = lenght_seond
+    result = 0
+    for index in range(count):
+        result += first_vector[index] * second_vector[index]
+    return result
