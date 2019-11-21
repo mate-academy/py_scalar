5078368ed45c8b66cd87ce306eaf3f82e5309af1
d1makov <makovskyj.danylo@gmail.com>
2019-11-21 13:38:55 +0200

    without using numpy module

--- scalar.py
+++ scalar.py
@@ -1,7 +1,6 @@
 """
 docstring
 """
-import numpy as np
 
 
 def scalar_product(list_one, list_two):
@@ -11,9 +10,8 @@ def scalar_product(list_one, list_two):
     :param list_two:
     :return:
     """
+    product = 0
     longer_list, shorter_list = max(list_one, list_two), min(list_one, list_two)
-    if len(longer_list) != len(shorter_list):
-        for _ in range(len(longer_list)-len(shorter_list)):
-            shorter_list.append(0)
-
-    return np.dot(longer_list, shorter_list)
+    for index, item in enumerate(shorter_list):
+        product += longer_list[index] * item
+    return product
