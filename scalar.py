0001e50c0906ee0af97e5f45ed0fc1d98a69c5b9
d1makov <makovskyj.danylo@gmail.com>
2019-11-21 13:28:44 +0200

    pass

--- scalar.py
+++ scalar.py
@@ -1,6 +1,19 @@
-from typing import List
+"""
+docstring
+"""
+import numpy as np
 
 
-def scalar_product(a: List[float], b: List[float]) -> List[float]:
-    return 0
+def scalar_product(list_one, list_two):
+    """
 
+    :param list_one:
+    :param list_two:
+    :return:
+    """
+    longer_list, shorter_list = max(list_one, list_two), min(list_one, list_two)
+    if len(longer_list) != len(shorter_list):
+        for _ in range(len(longer_list)-len(shorter_list)):
+            shorter_list.append(0)
+
+    return np.dot(longer_list, shorter_list)
--- test_scalar.py
+++ test_scalar.py
@@ -1,13 +1,28 @@
+"""
+docstring
+"""
 import scalar
 
 
 def test_scalar():
+    """
+
+    :return:
+    """
     assert scalar.scalar_product([1, 2, 3], [1, 2, 3]) == 14
 
 
 def test_empty():
+    """
+
+    :return:
+    """
     assert scalar.scalar_product([], []) == 0
 
 
 def test_different():
+    """
+
+    :return:
+    """
     assert scalar.scalar_product([1, 1], [1, 1, 1]) == 2
