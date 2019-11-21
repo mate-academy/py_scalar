b43574c4561fddd2ea24126a201dc28510cf80b5
Oleh_M <morhaliuk@gmail.com>
2019-11-21 12:46:32 +0200

    Solved

--- scalar.py
+++ scalar.py
@@ -1,6 +1,12 @@
-from typing import List
+"""Docstring"""
 
+def scalar_product(vec_1, vec_2):
+    """Returns a scalar product of vec_1 and vec_2"""
+    s_product = 0
+    for idx, elem in enumerate(vec_2):
+        try:
+            s_product += elem * vec_1[idx]
+        except IndexError:
+            return s_product
 
-def scalar_product(a: List[float], b: List[float]) -> List[float]:
-    return 0
-
+    return s_product
