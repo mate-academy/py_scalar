04cade0d6e6513d57bb7593b700f91190b9f8900
alexander <earthhuman41@gmail.com>
2019-11-22 01:05:10 +0200

    implemented scalar_product function

--- scalar.py
+++ scalar.py
@@ -1,6 +1,18 @@
+"""
+scalar_product
+"""
 from typing import List
 
 
-def scalar_product(a: List[float], b: List[float]) -> List[float]:
-    return 0
+def scalar_product(list_a: List[float], list_b: List[float]) -> float:
+    """
 
+    :param list_a: [float]
+    :param list_b: [float]
+    :return: float scalar product
+    """
+    result = .0
+    for i, item in enumerate(list_a):
+        result += item * list_b[i]
+
+    return result
