cf9d9b4b96931eea8b5be52c4a8be27d88f11f11
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-21 11:22:09 +0200

    v1

--- scalar.py
+++ scalar.py
@@ -1,6 +1,12 @@
-from typing import List
+"""doc"""
 
 
-def scalar_product(a: List[float], b: List[float]) -> List[float]:
-    return 0
+def scalar_product(first, second):
+    """scalar product"""
 
+    prod = 0
+    iteration = len(first) if len(first) <= len(second) else len(second)
+    for ind in range(iteration):
+        prod += first[ind] * second[ind]
+
+    return prod
