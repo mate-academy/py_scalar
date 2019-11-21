ba0ce228c91aa1e42b5ed97ee6a8a1fc574d055d
ValentinaLV <tina.lysenok@gmail.com>
2019-11-21 19:24:51 +0200

    added Denys solution

--- scalar.py
+++ scalar.py
@@ -19,3 +19,10 @@ def scalar_product(frst_vector: List[float], sec_vector: List[float]) -> float:
     else:
         return 0
     return scalar_prd
+
+
+def scalar_product_den_solution(frst_vector: List[float], sec_vector: List[float]) -> float:
+    sum_result = 0
+    for el1, el2 in zip(frst_vector, sec_vector):
+        sum_result += el1 * el2
+    return sum_result
