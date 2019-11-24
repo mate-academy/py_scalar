d5288bb84935bb9cde83d05d02f87301288ec7c1
Andrii <andrewsotnikov88@gmail.com>
2019-11-24 20:27:51 +0200

    Scalar product calculation

--- scalar.py
+++ scalar.py
@@ -1,6 +1,7 @@
+"""Find scalar product of two vectors"""
 from typing import List
 
 
-def scalar_product(a: List[float], b: List[float]) -> List[float]:
-    return 0
-
+def scalar_product(sequence_a: List[float], sequence_b: List[float]) -> List[float]:
+    """Dot product calculation"""
+    return sum([k*m for (k, m) in zip(sequence_a, sequence_b)])
