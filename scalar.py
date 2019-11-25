2c09695caad7f7d96446aa20aba07067079f94d3
Andrii <andrewsotnikov88@gmail.com>
2019-11-25 21:19:29 +0200

    fixed mypy

--- scalar.py
+++ scalar.py
@@ -2,6 +2,6 @@
 from typing import List
 
 
-def scalar_product(sequence_a: List[float], sequence_b: List[float]) -> List[float]:
+def scalar_product(sequence_a: List[int], sequence_b: List[int]) -> int:
     """Dot product calculation"""
     return sum([k*m for (k, m) in zip(sequence_a, sequence_b)])
